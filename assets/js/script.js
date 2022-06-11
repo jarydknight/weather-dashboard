cff7540bbef3e542e9d5d0f4e79d5cbfb1d8fb76
Jaryd Knight <jaryd.knight@hotmail.com>
2022-06-11 17:01:47 -0400

    create weather data object to store all of the data to be rendered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 // API Key - RIP privacy since can't use back end for API calls yet
 const apiKey = "312b5b9a9239037b60c85af07d3ebfbe";
 
+// Search Button Element
+const searchButtonEl = $(".search-button");
+
+// Search Bar Element
+const searchBarEl = $(".search-bar");
+
 // Function to make API calls to get weather for location
 const getWeather = (coordinate) => {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.long}&units=metric&exclude=minutely,hourly,alerts&appid=${apiKey}`).then(res => {
-    res.json().then(data => {
-        console.log(data);
+        res.json()
+        .then(data => {
+            let weatherData = {
+                current: {
+                    temp: data.current.temp,
+                    wind: data.current.wind_speed,
+                    humidity: data.current.humidity,
+                    uvi: data.current.uvi
+                },
+                fiveDayForecast: [data.daily[0], data.daily[1], data.daily[2], data.daily[3], data.daily[4]]
+            };
+            console.log(weatherData);
         })
     })
 }
@@ -14,7 +30,9 @@ const getWeather = (coordinate) => {
 const getCoordinates = (location) => {
     let coordinate;
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`).then(res => res.json().then(data => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`)
+    .then(res => res.json()
+    .then(data => {
         coordinate = {
             lat: data[0].lat,
             long: data[0].lon
@@ -28,10 +46,8 @@ const unixToDate = (unixTime) => {
     return date;
 }
 
+const renderData = (weatherData) => {
+
+}
 
 getCoordinates("portland")
-
-
-// setTimeout(function() {
-//     console.log(coordinate)
-// }, 5000)
